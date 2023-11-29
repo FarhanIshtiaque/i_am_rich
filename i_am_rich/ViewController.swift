44f2d040838c4619e130cffd6a2dbfaada089aa5
FarhanIshtiaque <rexfarhan@gmail.com>
2023-11-29 14:17:53 +0600

    Initial Commit

---
+++ i_am_rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i_am_rich
+//
+//  Created by Md Farhan Ishtiaque on 29/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
